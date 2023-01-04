8661d127dc0a7c8f3904143810008ab666ee1f90
Erickson <erickson.tulio@academico.ifpb.edu.br>
2023-01-04 08:42:32 -0300

    feat: Criado arquitetura modular do projeto

---
+++ backend/sportive/src/main/java/br/com/quattys/sportive/SportiveApplication.java
@@ -0,0 +1,13 @@
+package br.com.quattys.sportive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportiveApplication.class, args);
+	}
+
+}
---
+++ backend/sportive/src/test/java/br/com/quattys/sportive/SportiveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.quattys.sportive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
